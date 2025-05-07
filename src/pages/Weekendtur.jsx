d35608d022a5b46ed5f5454e8c9062165cbc2316
Mathias <math35x2@mcdmstudent.dk>
2025-05-07 11:17:11 +0200

    Made Weekend trip

--- src/pages/Weekendtur.jsx
+++ src/pages/Weekendtur.jsx
@@ -1,11 +1,13 @@
 
 
 import Header from "../components/header/Header";
+import Weekend from "../components/weekend/Weekend.jsx";
 
 export default function weekendtur() {
   return (
     <>
       <Header page="weekendtur" />
+      <Weekend />
     </>
   );
 }
