38fc6c067f00e84b27b8b5b2dc6c3463eee91ac4
vIctorianicol <victorianicolenco04@gmail.com>
2025-05-07 11:31:04 +0200

    victo

--- src/pages/aktiviteter.jsx
+++ src/pages/aktiviteter.jsx
@@ -1,13 +1,11 @@
-
-
-
-
 import Header from "../components/header/Header";
+import ActivityComponent from "../components/activitycomponent/ActivityComponent";
 
 export default function aktiviteter() {
   return (
     <>
       <Header page="aktiviteter" />
+      <ActivityComponent page="aktiviteter" />
     </>
   );
 }
