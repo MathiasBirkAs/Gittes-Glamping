74452ba7a0807bf28f4cf89355e6766bbc2c6dcf
KongenOliver <oliv6479@mcdmstudent.dk>
2025-05-07 11:36:27 +0200

    update

---
+++ src/components/feedback/Feedback.jsx
@@ -0,0 +1,15 @@
+import styles from "./feedback.module.css";
+/* import { feedback } from "../../data/feedback"; */
+
+export default function Feedback() {
+  /* const people = feedback;
+  const listItems = feedback.map((person) => (
+    <li key={person.id}>
+      <p>{person.name + ", " + person.age + " år"}</p>
+      <p>Har været på: {person.activity}</p>
+      <p>{person.review}</p>
+    </li>
+  )); */
+  /* return <ul className={styles.feedbackContainer}>{listItems}</ul>; */
+  return <section className={styles.feedbackContainer}></section>;
+}
