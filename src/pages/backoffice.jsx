3f05d2f726db5837511fcf0452a617786ea144fb
Mathias <math35x2@mcdmstudent.dk>
2025-05-08 10:17:11 +0200

    little change cuz i made smth that didnt work

--- src/pages/backoffice.jsx
+++ src/pages/backoffice.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useAuth } from "../hooks/useAuth";
+// import { useAuth } from "../hooks/useAuth";
 import { Navigate } from "react-router-dom";
 
 export default function Backoffice() {
